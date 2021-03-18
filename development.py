7a4900a7c77b30cb67c35954bdced5c2261a0625
James Walden <prussian1740@gmail.com>
2021-03-18 10:09:14 -0500

    first python class dev

---
+++ development.py
@@ -0,0 +1,11 @@
+## James Walden - Python Class ##
+class Python:
+	def __init__(self, name, health)
+	self.name = name
+	self.health = health
+	
+    def takeDamage(self):
+    self.health = -= 5
+
+
+python1 = Python(" ", 150)

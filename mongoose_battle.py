e71dd271ed99dcee653e236b6601a2d082c63ffd
Calvin Kling <klingcal@gmail.com>
2021-03-18 16:06:11 -0700

    battle function updates

--- mongoose_battle.py
+++ mongoose_battle.py
@@ -1,40 +1,49 @@
-from gooseClass import pythonAttack
-from gooseClass import Mongoose
-from jwalden2019 import Python
-from jwalden2019 import Mongoose
-from jwalden2019 import mongoose_bite_attack
-from jwalden2019 import mongoose_claw_attack
-# from python-rpg.py import main_menu
-# from python-rpg.py import victory_message
-# from python-rpg.py import defeat_message
-# from python-rpg.py import view_stats
+
+
+
+
+from gooseClass import pythonAttack, Mongoose
+from jwalden2019 import Python, mongoose_bite_attack, mongoose_claw_attack
+from python_rpg import main_menu, victory_message, defeat_message, view_stats
+
+
+char1 = Mongoose("Crazy Ricky", 150)
+char2 = Python("Big Johnson", 150)
 
 def run_away():
     print("You run away!")
     exit()
 
-def battleOptions():
-    option = int(input("""
-    A wild Python has appeared!
-    What would you like to do?
+def main_menu():
+    message = int(input("""
+    What would you like to do?\n\n
+    1. Bite the Python
+    2. Play dead...
+    3. Eat a berry
+    4. View Mongoose stats
+    5. Run away\n
+    ********************
     """))
-    while option != "q":
-        main_menu()
-        if option == 1:
-            mongoose_bite_attack()            
+    while message != "q":        
+        if message == 1:
+            mongoose_bite_attack(char1, char2)            
             main_menu()
-        elif option == 2:
-            mongoose_playdead_attack()
+        elif message == 2:
+            mongoose_claw_attack(char1, char2)
             main_menu()
-        elif option == 3:
-            eat_berry()
+        elif message == 3:
+            eat_berry(char1)
             main_menu()
-        elif option == 4:   
-            view_stats()
+        elif message == 4:   
+            view_stats(char1)
             main_menu()
-        elif option == 5:
+        elif message == 5:
             run_away()
         else:
             return print("Please input a valid option.")
+            main_menu()
 
-battleOptions()
+
+
+print("You are a Mongoose. A wild Python has appeared!")
+main_menu()

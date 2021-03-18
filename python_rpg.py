22f1f8dbbeadc6eaa34bcca64522384aded35e87
Calvin Kling <klingcal@gmail.com>
2021-03-18 16:06:23 -0700

    commented out

--- python_rpg.py
+++ python_rpg.py
@@ -2,16 +2,16 @@
 # main menu - attack, play dead, eat berry, run away
 
 def main_menu():
-    message = """
+    message = int(input("""
     What would you like to do?\n\n
     1. Bite the Python
     2. Play dead...
     3. Eat a berry
-    3. View Mongoose stats
-    4. Run away\n
+    4. View Mongoose stats
+    5. Run away\n
     ********************
-    """
-    return print(message)
+    """))
+    return message
 
 
 def victory_message():

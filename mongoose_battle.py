20cc689ee7b16c7d2c1d220574dc01686e51719f
Calvin Kling <klingcal@gmail.com>
2021-03-18 15:05:22 -0700

    added imports

--- mongoose_battle.py
+++ mongoose_battle.py
@@ -1,26 +1,37 @@
+from gooseClass import pythonAttack
+from gooseClass import Mongoose
+from jwalden2019 import Python
+from jwalden2019 import Mongoose
+from jwalden2019 import mongoose_bite_attack
+from jwalden2019 import mongoose_claw_attack
+# from python-rpg.py import main_menu
+# from python-rpg.py import victory_message
+# from python-rpg.py import defeat_message
+# from python-rpg.py import view_stats
+
 def run_away():
     print("You run away!")
     exit()
 
 def battleOptions():
-    print("""
+    option = int(input("""
     A wild Python has appeared!
     What would you like to do?
-    """)
+    """))
     while option != "q":
-        # main_menu()
+        main_menu()
         if option == 1:
             mongoose_bite_attack()            
-
+            main_menu()
         elif option == 2:
-            #play dead attack
-
+            mongoose_playdead_attack()
+            main_menu()
         elif option == 3:
-            pass
-    # call function to eat berry for health
+            eat_berry()
+            main_menu()
         elif option == 4:   
-            pass         
-    # view stats function
+            view_stats()
+            main_menu()
         elif option == 5:
             run_away()
         else:

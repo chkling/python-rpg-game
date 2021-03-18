17781d3208da35a8f113c2cf886fdee0ef5cf6a8
Calvin Kling <klingcal@gmail.com>
2021-03-18 11:02:57 -0700

    if while options

--- python-rpg.py
+++ python-rpg.py
@@ -1,1 +1,14 @@
-randy chong
+
+option = ""
+while option != 5:
+    main_menu()
+    if option == 1:
+        pass
+    if option == 2:
+        pass
+    if option == 3:
+        pass
+    if option == 4:
+        pass
+    if option == 5:
+        pass

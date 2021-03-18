0dff327a2b90ab69ce9917aa7a0a878c5b50601f
randy chong <randychong27@yahoo.com>
2021-03-18 09:55:01 -0500

    initial commit

--- python-rpg.py
+++ python-rpg.py
@@ -1,1 +1,12 @@
-randy chong
+# mongoose vs python
+# main menu - attack, play dead, eat berry, run away
+
+def main_menu():
+    message = """
+    A wild Python has appeared!\n
+    What would
+    1. Bite the Python\n
+    2. Play dead...\n
+    3. Eat a berry
+    4. Run away
+    """
